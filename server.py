fb7c7e22341ca9f86386b39219f50afa7c26a5c5
spadiator <mike.spadier@gmail.com>
2025-01-17 08:54:20 -0600

    Add files via upload

--- server.py
+++ server.py
@@ -11,15 +11,7 @@ def read_root():
 
 @app.get("/currently-playing")
 def get_current_song():
-    SPOTIFY_API_URL = "https://api.spotify.com/v1/me/player/currently-playing"
-
-    # Your actual Spotify credentials
-    SPOTIFY_CLIENT_ID = "fa74ddfa85064b4a9cd807d1b596e3d6"
-    SPOTIFY_CLIENT_SECRET = "09e10109bd5d42e493d7751f37d409fc"
-    SPOTIFY_REFRESH_TOKEN = "AQBPdyZ42yIk6HftPQLOkA8ehvKzXjEvkoPe9SnUILl_u_kC7tl7hDSLAAbiY5vTABdcBkZaqpdfUH-p8s4MyHCtBVNnPyg2-88srqu7nkVQ_3YAlWk7rgbTzc2rdz5rcwU"
-
-    # Use the refresh token function to get a fresh access token
-    access_token = refresh_spotify_token(SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET, SPOTIFY_REFRESH_TOKEN)
+    access_token = refresh_spotify_token()
     if not access_token:
         return {"error": "Failed to refresh token"}
 
