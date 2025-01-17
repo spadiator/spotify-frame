2cce5d30c319d39efe8ddc7e4cb65f574d10ee89
spadiator <mike.spadier@gmail.com>
2025-01-17 11:02:34 -0600

    Add files via upload

--- server.py
+++ server.py
@@ -85,5 +85,5 @@ def spotify_callback(code: str, state: str):
         raise HTTPException(status_code=400, detail="Spotify authorization failed")
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 10000))
+    port = int(os.getenv("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
